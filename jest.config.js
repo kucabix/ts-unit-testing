20445960cb1b2b091a5da0a600cf703082cb1813
Maciej Kucaba <kucabix@Mac-mini-Maciej.local>
2020-10-29 12:00:18 +0100

    Started TS unit testing course

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest'
-    },
-    testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    verbose: true
+  roots: ['<rootDir>/src'],
+  transform: {
+      '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  verbose: true,
+  moduleDirectories: ['node_modules', 'src']
 }
